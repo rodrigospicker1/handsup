c361a4ef0482391c9335b0e70a3dbd925853e02d
Rodrigo Spicker Reis Silva <r.spicker13@gmail.com>
2025-05-07 19:43:28 -0300

    Rodrigo - 30/04 - Criando estrutura base do projeto

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!session()->has('user')) {
+            return redirect('/login'); // se não logado, manda pro login
+        }
+
+        return $next($request); // se logado, continua
+    }
+}
